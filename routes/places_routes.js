3c06370c0a5466d5fd3a423cd3aeb86f73881dc5
JRocken <jp.rockenmacher@gmail.com>
2018-09-23 21:35:20 -0600

    db linked to readme

--- routes/places_routes.js
+++ routes/places_routes.js
@@ -3,40 +3,40 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
-router.get("/", (request, response, next) => {
+router.get("/", (req, res, next) => {
     queries.listPlaces().then(places => {
-        response.json({places});
+        res.json({places});
     }).catch(next);
 });
 
 
-router.get("/:id", (request, response, next) => {
-    queries.readPlace(request.params.id).then(place => {
+router.get("/:id", (req, res, next) => {
+    queries.readPlace(req.params.id).then(place => {
         place
-            ? response.json({place})
-            : response.status(404).json({message: 'place not found'})
+            ? res.json({place})
+            : res.status(404).json({message: 'place not found'})
     }).catch(next);
 });
 
 
-router.post("/", (request, response, next) => {
-    console.log(request.body)
-    queries.createPlace(request.body).then(place => {
-        response.status(201).json({place});
+router.post("/", (req, res, next) => {
+    console.log(req.body)
+    queries.createPlace(req.body).then(place => {
+        res.status(201).json({place});
     }).catch(next);
 });
 
-router.delete("/:id", (request, response, next) => {
-    queries.deletePlace(request.params.id).then(() => {
-        response.status(200).json({deleted: true});
+router.delete("/:id", (req, res, next) => {
+    queries.deletePlace(req.params.id).then(() => {
+        res.status(200).json({deleted: true});
     }).catch(next);
 });
 
-router.put("/:id", (request, response, next) => {
-    console.log(request.params.id)
-    console.log(request.body)
-    queries.updatePlace(request.params.id, request.body).then(place => {
-        response.json({place});
+router.put("/:id", (req, res, next) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    queries.updatePlace(req.params.id, req.body).then(place => {
+        res.json({place});
     }).catch(next);
 });
 
